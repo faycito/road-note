644ccfa8cc401765cdaf0a8b8058db9b7e64f0aa
Ismael <ismaelalexandermc@gmail.com>
2018-01-12 01:23:21 -0500

    image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,3 @@
-import { LoginPage } from './../pages/login/login';
-import { GeneralNewsPage } from './../pages/news/general-news/general-news';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -10,13 +8,16 @@ import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { NewsPage } from '../pages/news/news';
 import { HelpPage } from '../pages/help/help';
+import { Camera } from '@ionic-native/camera';
+import { LoginPage } from './../pages/login/login';
 import { AboutUsPage } from '../pages/about-us/about-us';
+import { GeneralNewsPage } from './../pages/news/general-news/general-news';
 import { FinesAndPenaltiesPage } from '../pages/fines-and-penalties/fines-and-penalties';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { RoadBlockAgePage } from '../pages/road-block/road-blockage';
 import { SendReportPage } from '../pages/send-report/send-report';
+import { RoadBlockAgePage } from '../pages/road-block/road-blockage';
 
 @NgModule({
   declarations: [
@@ -53,6 +54,7 @@ import { SendReportPage } from '../pages/send-report/send-report';
   providers: [
     StatusBar,
     SplashScreen,
+    Camera,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/send-report/send-report.ts
+++ src/pages/send-report/send-report.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 
 @Component({
@@ -11,12 +12,28 @@ import { AlertController } from 'ionic-angular';
 export class SendReportPage {
 
     complainForm: FormGroup;
+    imageFile;
 
     constructor(
         private formBuilder : FormBuilder,
-        private _alertCtrl: AlertController
+        private _alertCtrl: AlertController,
+        private _camera: Camera    
     ){
         this.setFomr();
+    }
+
+    getGallery(){
+        let options : CameraOptions = {
+            destinationType: this._camera.DestinationType.DATA_URL,
+            sourceType: this._camera.PictureSourceType.PHOTOLIBRARY
+        };
+
+        this._camera.getPicture(options)
+            .then( data => {
+                this.imageFile = 'data:image/jpeg;base64,' + data;
+            }, error => {
+                console.log(error);
+            })
     }
 
     setFomr(): void{
@@ -24,15 +41,18 @@ export class SendReportPage {
             to: ['', Validators.required],
             subject: ['', Validators.required],
             content: ['', Validators.required]
-        })
+        });
+        this.imageFile = '';
     }
     onSubmit(data: any){
+        if(this.imageFile) { data.image = this.imageFile};
+
         console.log(data);
-        this.setFomr();
         let alert = this._alertCtrl.create({
             title: 'Send Form',
             subTitle: 'Your form was send successfuly'
         });
         alert.present();
+        this.setFomr();
     }
 }
